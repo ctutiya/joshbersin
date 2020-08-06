36214b860f9ee298c815c9d132562a5d97b8f593
ctutiya <67704827+ctutiya@users.noreply.github.com>
2020-08-06 10:15:52 -0700

    Update content-news.php

--- wp-content/themes/mouche-child/template-parts/content-news.php
+++ wp-content/themes/mouche-child/template-parts/content-news.php
@@ -1,6 +1,6 @@
 <div class="p-t-40 p-b-40 news-item">
   <a href="<?php the_permalink(); ?>" class="color-black">
-    <h2 class="small m-b-20 width-800"><?php the_title(); ?></h2>
+    <h2 class="small m-b-20 width-780"><?php the_title(); ?></h2>
   </a>
   <div class="color-black font-16 m-b-20"><?php the_excerpt(); ?></div>
   <a href="<?php the_permalink(); ?>" class="btn small primary">READ MORE <i class="m-l-5 icon-arrow_right_alt"></i></a>
